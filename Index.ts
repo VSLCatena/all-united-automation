9aec24a4641f67d6a1d16802087a6c9bf86ca881
jannyboy313 <janvanoverbeek12@gmail.com>
2023-12-04 19:34:23 +0100

    Added cron

--- Index.ts
+++ Index.ts
@@ -1,3 +1,5 @@
+import cron from 'cron';
+import axios from 'axios';
 import dotenv from "dotenv";
 import express from "express";
 import SynchronizationRouter from "./routes/SynchronizationRouter";
@@ -12,6 +14,29 @@ app.get("/health", (req, res) => {
 
 app.use('/synchronise', SynchronizationRouter);
 
+// Runs every day at midnight
+const cronJob = new cron.CronJob('0 0 0 * * *', () => {
+    let retryCount = 0;
+    const executeCronJob = async () => {
+        try {
+            await axios.get('http://localhost:3000/synchronise');
+        } catch (error) {
+            console.error('Error executing cron job:', error);
+
+            retryCount++;
+            if (retryCount <= 3) {
+                console.log(`Retrying... (Attempt ${retryCount})`);
+                executeCronJob();
+            } else {
+                console.error('Cron job failed after 3 retries');
+                // TODO Send email to admin
+            }
+        }
+    }
+    executeCronJob();
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`API up and running at http://localhost:${process.env.PORT}`);
+    cronJob.start();
 });
